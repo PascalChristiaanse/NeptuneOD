0f5fcc287e028e43b66cc30dbfb921c0e53c6a79
PascalChristiaanse <pascal.christiaanse@gmail.com>
2026-05-17 22:55:21 +0200

    remove deprecated function

--- src/orbitdet/estimation/__init__.py
+++ src/orbitdet/estimation/__init__.py
@@ -1,4 +1,3 @@
 from .estimators import get_estimatable_parameters
-from .observation_generation import generate_observations
 
-__all__ = ["generate_observations", "get_estimatable_parameters"]
+__all__ = ["get_estimatable_parameters"]
